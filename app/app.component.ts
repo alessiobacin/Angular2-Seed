6ae2a33836e8d59b1c5227a6db3eee9d644253c7
Alessio Bacin <alessiobacin@Alessios-MacBook-Pro.local>
2016-08-05 08:10:02 +0200

    Base Angular2

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App <test></test></h1>'
+})
+export class AppComponent {
+
+}
